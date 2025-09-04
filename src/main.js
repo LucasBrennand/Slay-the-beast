926810ca20508d4ec27f1b082393bdea50e9f513
Lucas <lucasbrennand.barbosa@gmail.com>
2025-09-04 20:40:28 -0300

    Making atk btns

--- src/main.js
+++ src/main.js
@@ -3,4 +3,24 @@ const attackBtn = document.querySelector("#attack-btn")
 const magicBtn = document.querySelector("#magic-btn")
 const itemsBtn = document.querySelector("#items-btn")
 const giveUpBtn = document.querySelector("#giveup-btn")
+const playersBtnsContainer = document.querySelector("#players-btns-container")
+
+const homeBtns = () => {
+    playersBtnsContainer.innerHTML = `
+    <button id="attack-btn">Attack</button>
+            <button id="magic-btn">Magic</button>
+            <button id="items-btn">Items</button>
+            <button id="giveup-btn">Give Up</button>
+    `
+}
+
+attackBtn.addEventListener("click", () => {
+    playersBtnsContainer.innerHTML = `
+    
+            <button id="attack-btn">Raging Sword</button>
+            <button id="magic-btn">Slash</button>
+            <button id="items-btn">Reckless strike</button>
+            <button id="giveup-btn" onClick=homeBtns()>Go Back</button>
+    `
+})
 
