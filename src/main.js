2c04492573bdfe2589402ddbfb51b4b4e55d79ea
Lucas <lucasbrennand.barbosa@gmail.com>
2025-09-04 19:41:51 -0300

    First commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+const playerUIContainer = document.getElementById("player-ui-container")
+const attackBtn = document.querySelector("#attack-btn")
+const magicBtn = document.querySelector("#magic-btn")
+const itemsBtn = document.querySelector("#items-btn")
+const giveUpBtn = document.querySelector("#giveup-btn")
+
